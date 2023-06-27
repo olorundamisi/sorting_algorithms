6b941a7e5a0da5fad864e5418d2be83601882270
Olorundamisi Dunmade <olorundamisi1@gmail.com>
2023-06-28 00:04:58 +0100

    `shell_sort`

---
+++ 100-shell_sort.c
@@ -0,0 +1,54 @@
+/*
+ * File: 100-shell_sort.c
+ * Author: Olorundamisi Dunmade <github.com/olorundamisi>
+ */
+
+#include "sort.h"
+
+/**
+ * swap_ints - Swap two integers. This procedure works for array elements
+ *			   and regular ints alike.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - Sort an array of integers in non-decreasing order using the
+ *              shell sort algorithm.
+ * @array: The array of integers to sort.
+ * @size: The size or length of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
+
+	for (; gap >= 1; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			j = i;
+			while (j >= gap && array[j - gap] > array[j])
+			{
+				swap_ints(array + j, array + (j - gap));
+				j -= gap;
+			}
+		}
+		print_array(array, size);
+	}
+}
